29af52363e6a9595a4f7c124c8b853f2b83698a9
lugril <lugril.m@gmail.com>
2023-12-20 17:22:43 +0100

    preps for using less ram

--- day5_seed_fertilizer/d5_p2.py
+++ day5_seed_fertilizer/d5_p2.py
@@ -1,4 +1,8 @@
 def get_destination (destination, sources, reach, to_map):
+    print(type(to_map[0]))
+    print(type(to_map))
+    print(type(sources[0]))
+    print(type(sources))
     mapped = to_map.copy()
     for cnt in range(len(to_map)):
         for position in range(len(sources)):
@@ -12,6 +16,18 @@ def convert_map(map):
     for line in range(len(map)):
         map[line] = map[line].split(" ")
     return(map)
+
+def create_seeds_from_range (start, reach):
+    seeds = []
+    start = int(start)
+    reach = int(reach)
+    print("adding seedrage")
+    print("{}-{}".format(start, reach))
+    for each in range((reach)):
+        seeds.append(start+each)
+        print(seeds)
+    return(seeds)
+
 
 print("reading file")
 input = open("input_test.txt", "r")
@@ -31,21 +47,31 @@ _, seed_ranges = seed_ranges.split(": ")
 print("splitting seedranges more")
 seed_ranges = seed_ranges.split(" ")
 
-tmp_result = 0
-seeds = []
+result = []
+
 for seed in range (0,len(seed_ranges),2):
-    print(seeds)
-    print("adding seedrage {}/{}".format(seed/2,len(seed_ranges)/2))
-    print("{}-{}".format(seed_ranges[seed], seed_ranges[seed+1]))
-    #print(seed_ranges[seed])
-    for reach in range(int(seed_ranges[seed+1])):
-        seeds.append(int(seed_ranges[seed])+reach)
+    seed_range = create_seeds_from_range(seed_ranges[seed],seed_ranges[seed+1])
+    result.append(seed_range)
+    #hier wird eine liste an listen erstellt: soll liste an seeds sein
+    #hier ist aktueller fehler
+    print("result", result)
+
+#tmp_result = 0
+#for seed in range (0,len(seed_ranges),2):
+#    seeds = []
+#    print(seeds)
+#    print("adding seedrage {}/{}".format(seed/2,len(seed_ranges)/2))
+#    print("{}-{}".format(seed_ranges[seed], seed_ranges[seed+1]))
+#    #print(seed_ranges[seed])
+#    for reach in range(int(seed_ranges[seed+1])):
+#        seeds.append(int(seed_ranges[seed])+reach)
+        
     
 
 
 #print("mappings:")
-print("seedcount", len(seeds))
-result = seeds.copy()
+#print("seedcount", len(seeds))
+#result = seeds.copy()
 block = 0
 for mapping in almanac:
     block += 1
@@ -58,7 +84,7 @@ for mapping in almanac:
         source.append(int(entry[1]))
         reach.append(int(entry[2]))
     result = get_destination(destination,source,reach,result)
-    #print(result) 
+    print(result) 
 
 #print(sorted(result))
 print("nearest location:", sorted(result)[0])
